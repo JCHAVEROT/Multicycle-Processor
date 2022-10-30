4fcf19972e5d6db895bb9d1d023bfbc37007f0ad
Jeremy Chaverot <100281310+jchaverot@users.noreply.github.com>
2022-10-30 20:16:00 +0100

    try to fix PC 2

--- template/vhdl/PC.vhd
+++ template/vhdl/PC.vhd
@@ -21,13 +21,13 @@ architecture synth of PC is
     signal next_addr : std_logic_vector(15 downto 0);
 begin
 
-    next_addr <= std_logic_vector(signed(curr_addr) + signed(imm)) when add_imm = '1' else
-                 (imm(13 downto 0) & "00") when sel_imm = '1' else
+    next_addr <= std_logic_vector(unsigned(curr_addr) + unsigned(imm)) when add_imm = '1' else
+                 imm(13 downto 0) & "00" when sel_imm = '1' else
                  a when sel_a = '1' else 
                  std_logic_vector(unsigned(curr_addr) + to_unsigned(4, 16));
 
     -- synchronous process to go to the next address with asynchronous reset
-    process(clk, reset_n)
+    process(clk, reset_n, next_addr)
     begin
         if (reset_n = '0') then
             curr_addr <= (others => '0');
@@ -35,7 +35,8 @@ begin
         elsif rising_edge(clk) then
             if (en = '1') then
                 curr_addr <= next_addr;
-                addr(15 downto 0) <= next_addr;
+                addr <= X"0000" & next_addr;
+                addr(1 downto 0) <= "00";
             end if;
         end if;
     end process;
